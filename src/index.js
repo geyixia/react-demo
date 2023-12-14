72e5797e68f6e9048796c88847430825904eec69
葛东亚 <gdy_work_9464@163.com>
2023-12-14 16:56:58 +0800

    hello jsx

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 
 // 创建react根对象
-const root = createRoot(document.querySelector('#root'))
+const root = createRoot(document.querySelector("#root"));
 
 root.render(
-  <h1>Hello React</h1>
-)
+  <div>
+    <h1>Hello jsx</h1>
+    <img
+      className=""
+      src="http://150.223.2.73:9103/api/file/view/6587e37c-e664-4088-91d0-9a76f7656264.png"
+      alt="hha"
+    />
+  </div>
+);
